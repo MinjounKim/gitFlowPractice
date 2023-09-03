013b6fc76e75dcfa9f9e5ee16e42dc78159bf629
김민준 <rlaalswns804@gmail.com>
2023-09-03 19:05:02 +0900

    restart

--- Master.ts
+++ Master.ts
@@ -1,16 +1,28 @@
 class Master {
     master: string | null;
     Dev: string | null;
-
+    feature1: string | null;
+    feature2: string | null;
     constructor() {
         this.master = null;
+        this.Dev = null;
+        this.feature1 = null;
+        this.feature2 = null;
     }
 
-    changeMaster() {
+    masterWorkSpace() {
         this.master = 'master-work1';
     }
 
-    DevWorkSpace() {
+    devWorkSpace() {
         this.Dev = 'dev-work ready to merge';
+    }
+
+    feature1WorkSpace() {
+        this.feature1 = 'feature/0001-work';
+    }
+
+    feature2WorkSpace() {
+        this.feature2 = 'feature/0002-work';
     }
 }

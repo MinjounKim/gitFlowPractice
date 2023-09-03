825677fa9f9a76d0eec5a0b482b8b1ec464bb5a6
김민준 <rlaalswns804@gmail.com>
2023-09-03 17:54:57 +0900

    feature/0001-work1

---
+++ Feature1.ts
@@ -0,0 +1,10 @@
+class Feature1 {
+    feature1:string|null;
+    constructor(){
+        this.feature1=null;
+    }
+
+    featureWorkSpace(){
+        this.feature1='feature/0001-work1';
+    }
+}
